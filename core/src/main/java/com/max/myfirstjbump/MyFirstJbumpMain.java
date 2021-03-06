5d71618a75bb2f899b71731989cb7f9837b78dea
maxkcy <maqmustafa@gmail.com>
2021-03-06 11:08:36 -0800

    jump + movement demo with jbump done

--- core/src/main/java/com/max/myfirstjbump/MyFirstJbumpMain.java
+++ core/src/main/java/com/max/myfirstjbump/MyFirstJbumpMain.java
@@ -3,6 +3,7 @@ package com.max.myfirstjbump;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -15,12 +16,17 @@ import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.dongbat.jbump.Collision;
+import com.dongbat.jbump.CollisionFilter;
 import com.dongbat.jbump.Item;
 import com.dongbat.jbump.Rect;
+import com.dongbat.jbump.Response;
 import com.dongbat.jbump.World;
 
 import java.awt.image.ImagingOpException;
@@ -37,9 +43,16 @@ public class MyFirstJbumpMain extends Game {
 	public TiledMap map;
 	public OrthogonalTiledMapRenderer mapRenderer;
 	public World<Entity> world;
-	public Item<Entity> myFirstItem;
 	public Array<Item> itemArray;
 	ShapeRenderer shapeDrawer;
+	public Vector3 touch;
+	Item<Entity> playerItem;
+	float angle;
+	float newX;
+	float newY;
+	boolean touching;
+	int acceleration = 5;
+	int velocity = 0;
 
 	@Override
 	public void create() {
@@ -70,13 +83,77 @@ public class MyFirstJbumpMain extends Game {
 				itemArray.add(item);
 
 			}
-
 		}
 
 
+		Entity player = new Entity(152, 40, 5, 5);
+		playerItem = new Item<>(player);
+		world.add(playerItem, player.position.x, player.position.y, player.width, player.height);
+		itemArray.add(playerItem);
+
 
 	}
+	CollisionFilter myFirstCollision = new CollisionFilter() {
+		@Override
+		public Response filter(Item item, Item other) {
+			if(other.userData instanceof Entity){return Response.touch;}
+			return null;
+		}
 
+	};
+	 public void movement() {
+		if(Gdx.input.isTouched()) {
+			touch = viewport.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+			angle = MathUtils.atan2(touch.y - playerItem.userData.position.y, touch.x - playerItem.userData.position.x)
+					* MathUtils.radiansToDegrees;
+			angle = (((angle % 360) + 360) % 360);
+			if(touch.x - playerItem.userData.position.x > 5 || touch.x - playerItem.userData.position.x < -5){
+				newX = playerItem.userData.position.x + (MathUtils.cosDeg(angle) * 5);
+			} else { newX = touch.x;}
+			if(touch.y - playerItem.userData.position.y > 5 || touch.y - playerItem.userData.position.y < -5){
+				newY = playerItem.userData.position.y + (MathUtils.sinDeg(angle) * 5);
+			} else {newY = touch.y;}
+			world.move(playerItem, newX, newY, myFirstCollision);
+			//last spot is here, this is the new spot so body can be told to move in direction
+			//playerItem.userData.position.x = newX;
+			//playerItem.userData.position.y = newY;
+			//fixed under
+			playerItem.userData.position.x = world.getRect(playerItem).x;
+			playerItem.userData.position.y = world.getRect(playerItem).y;
+			//System.out.println(playerItem.userData.position);
+
+		}
+	 }
+
+	 public void jump() {
+		 newX = playerItem.userData.position.x;
+		 newY = playerItem.userData.position.y;
+	 	Response.Result move = world.move(playerItem, newX, newY - 5, myFirstCollision);
+		 for (int i = 0; i < move.projectedCollisions.size(); i++) {
+			 Collision collision = move.projectedCollisions.get(i);
+			 System.out.println(collision);
+			 Object userData = collision.other.userData;
+			 //System.out.println(userData);
+			 if (userData instanceof Entity) {
+				 touching = true;
+				 if (Gdx.input.isTouched()) {
+					touch = viewport.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+					 /*angle = MathUtils.atan2(touch.y - playerItem.userData.position.y, touch.x - playerItem.userData.position.x)
+							 * MathUtils.radiansToDegrees;
+					 angle = (((angle % 360) + 360) % 360);*/
+					 velocity = 15;
+					 touching = false;
+					 newX = touch.x;
+				 }
+			 }
+		 }
+		 newY += velocity;
+		 world.move(playerItem, newX, newY, myFirstCollision);
+		 playerItem.userData.position.x = world.getRect(playerItem).x;
+		 playerItem.userData.position.y = world.getRect(playerItem).y;
+		 velocity -= acceleration;
+		 System.out.println(touching);
+	 }
 	@Override
 	public void render() {
 		Gdx.gl.glClearColor(0, 1, 1, 1);
@@ -99,10 +176,11 @@ public class MyFirstJbumpMain extends Game {
 
 				Rect rect = world.getRect(item);
 				shapeDrawer.rect(rect.x, rect.y, rect.w, rect.h);
-
 			}
 		}
 		shapeDrawer.end();
+		movement();
+		//jump();
 		super.render();
 	}
 
